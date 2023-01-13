965daa54e98e35996747cc6fc6bbdfbe80ac4211
bskim617 <bskim617@gmail.com>
2023-01-13 13:21:18 +0900

    2023/01/13

--- Ch07/src/main/java/kr/co/ch07/repository/User6Repo.java
+++ Ch07/src/main/java/kr/co/ch07/repository/User6Repo.java
@@ -1,5 +1,9 @@
 package kr.co.ch07.repository;
 
-public class User6Repo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kr.co.ch07.vo.User6VO;
+
+public interface User6Repo extends JpaRepository<User6VO, String> {
 
 }
--- Ch08/src/main/java/kr/co/ch08/service/User2Service.java
+++ Ch08/src/main/java/kr/co/ch08/service/User2Service.java
@@ -5,9 +5,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import kr.co.ch08.repository.User2Repo;
+import kr.co.ch08.security.MyUserDetails;
 import kr.co.ch08.vo.User2VO;
 
 @Service
@@ -15,6 +17,15 @@ public class User2Service implements UserDetailsService {
 	
 	@Autowired
 	private User2Repo repo;
+	
+	public void insertUser(User2VO vo) {
+		
+		//Spring Security 암호화 처리
+		BCryptPasswordEncoder passEncoder = new BCryptPasswordEncoder();
+		vo.setPass(passEncoder.encode(vo.getPass()));
+		
+		repo.save(vo);
+	}
 	
 	public void insertUser2() {
 		
@@ -28,11 +39,11 @@ public class User2Service implements UserDetailsService {
 		return repo.findUser2VOByUidAndPass(uid, pass);
 	}
 	
-	public void updateUser1() {
+	public void updateUser2() {
 		
 	}
 	
-	public void deleteUser1() {
+	public void deleteUser2() {
 		
 	}
 
@@ -40,17 +51,29 @@ public class User2Service implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		User2VO user = repo.findById(username).get();
+		System.out.println("user: "+user);
 		
 		if(user == null) {
 			System.out.println("user 없음...");
 			throw new UsernameNotFoundException(username);
 		}
 		
-		UserDetails userDts = User.builder()
+		/* UserDetails userDts = User.builder()
 							  .username(user.getUid())
 							  .password(user.getPass())
 							  .roles("ADMIN").build(); 
 		
-		return userDts;
+		return userDts; */
+		
+		MyUserDetails myUser = new MyUserDetails();
+		myUser.setUid(user.getUid());
+		myUser.setPass(user.getPass());
+		myUser.setName(user.getName());
+		myUser.setHp(user.getHp());
+		myUser.setAge(user.getAge());
+		myUser.setRdate(user.getRdate().toString());
+		
+		
+		return myUser;
 	}
 }
